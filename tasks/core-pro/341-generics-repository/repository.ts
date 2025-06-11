d60245ce6acf546d2aa516c3340ad4e2fe0e9145
bmichalczuk <barmic90@hotmail.com>
2025-06-12 00:41:35 +0200

    finish 341

--- tasks/core-pro/341-generics-repository/repository.ts
+++ tasks/core-pro/341-generics-repository/repository.ts
@@ -1,9 +1,16 @@
+import { table } from 'console';
 import { DataAccess } from './DataAccess.ts';
 
 export interface User {
   id: number;
   name: string;
   email: string;
+}
+
+export interface Product {
+  id?: number;
+  name: string;
+  price: number;
 }
 
 export class UserRepository {
@@ -35,12 +42,6 @@ export class UserRepository {
   }
 }
 
-export interface Product {
-  id?: number;
-  name: string;
-  price: number;
-}
-
 export class ProductRepository {
   private dataAccess: DataAccess;
 
@@ -70,4 +71,42 @@ export class ProductRepository {
   }
 }
 
-export class Repository<T extends { id?: number }> {}
+export class Repository<T extends Product | User> {
+  private dataAccess: DataAccess;
+  private tableName: 'users' | 'products';
+  constructor(dataAccess: DataAccess, tableName: 'users' | 'products') {
+    this.dataAccess = dataAccess;
+    this.tableName = tableName;
+  }
+
+  async getById(id: number) {
+    const query = `SELECT * FROM ${this.tableName} WHERE id = $1`;
+    const values = [id];
+    const res = await this.dataAccess.query<T>(query, values);
+    return res.rows[0];
+  }
+
+  async getAll() {
+    const query = `SELECT * FROM ${this.tableName}`;
+    const res = await this.dataAccess.query<T>(query);
+    return res.rows;
+  }
+
+  async insert(data: Omit<T, 'id'>) {
+    const { name } = data;
+    const price = 'price' in data ? data.price : undefined; // Handle User and Product differently
+    const email = 'email' in data ? data.email : undefined; // Handle User and Product differently
+    if (price) {
+      const query = `INSERT INTO ${this.tableName} (name, price) VALUES ($1, $2) RETURNING *`;
+      const values = [name, price];
+      const res = await this.dataAccess.query<T>(query, values);
+      return res.rows[0];
+    }
+    if (email) {
+      const query = `INSERT INTO ${this.tableName} (name, email) VALUES ($1, $2) RETURNING *`;
+      const values = [name, email];
+      const res = await this.dataAccess.query<User>(query, values);
+      return res.rows[0];
+    }
+  }
+}

ade1f42c996ba6697a2868adb4239052b4ed8212
bmichalczuk <barmic90@hotmail.com>
2025-06-20 22:24:17 +0200

    finish 411

--- tasks/core-pro/411-mapped-access/task.ts
+++ tasks/core-pro/411-mapped-access/task.ts
@@ -4,6 +4,20 @@ type PagesMap = {
   contact: string;
 };
 
-type PagesAccess = {};
+type PagesAccess = {
+  [K in keyof PagesMap]: boolean;
+};
 
-export function checkAccess(map: PagesMap): PagesAccess {}
+export function checkAccess(map: PagesMap): PagesAccess {
+  const access: PagesAccess = {
+    homepage: false,
+    about: false,
+    contact: false,
+  };
+  for (const key in map) {
+    if (map.hasOwnProperty(key)) {
+      access[key as keyof PagesAccess] = true;
+    }
+  }
+  return access;
+}

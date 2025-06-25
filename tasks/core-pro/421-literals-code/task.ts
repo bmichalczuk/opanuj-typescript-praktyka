35467f72283b04143a2a54a91a134f2920dca2e9
Bartosz Michalczuk <barmic90@hotmail.com>
2025-06-25 13:58:25 +0000

    finish 421

--- tasks/core-pro/421-literals-code/task.ts
+++ tasks/core-pro/421-literals-code/task.ts
@@ -1,5 +1,14 @@
-export type Code = null;
+type AlowedCodes = '000' | '001' | '010' | '100' | '011' | '101' | '110' | '111';
+export type Code = `${AlowedCodes}-${AlowedCodes}-${AlowedCodes}`;
+
+const binaryToDecimal = (binaryNumberString: string) => {
+  const decimalNumber = parseInt(binaryNumberString, 2);
+
+  return String(decimalNumber);
+};
 
 export function codeToDecimal(code: Code) {
-  throw new Error('Not implemented');
+  const binaryCodes = code.split('-');
+
+  return binaryCodes.map(binaryToDecimal).join('');
 }

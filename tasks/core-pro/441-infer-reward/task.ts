6b287d542e613b9280a2c03fbad6d9c9569c8648
bmichalczuk <barmic90@hotmail.com>
2026-01-21 12:58:21 +0100

    finish 441

--- tasks/core-pro/441-infer-reward/task.ts
+++ tasks/core-pro/441-infer-reward/task.ts
@@ -1,1 +1,12 @@
-export type RewardRadar = '🔍';
+type Raw<T extends string> = T extends `${string}[${infer Amount}$]${infer Rest}`
+  ? Raw<Rest> extends null
+    ? ExcludeZero<ToPositiveNumber<Amount>> extends never
+      ? null
+      : ExcludeZero<ToPositiveNumber<Amount>>
+    : Raw<Rest>
+  : null;
+
+type ToPositiveNumber<S extends string> = S extends `${infer N extends number}` ? N : never;
+
+type ExcludeZero<N extends number> = Exclude<N, 0>;
+export type RewardRadar<T extends string> = Raw<T> extends null ? null : `${Raw<T>}$`;

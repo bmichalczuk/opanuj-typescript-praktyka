00939337b1f45b73cb7c0b2e613a2b8eaf93340f
bmichalczuk <barmic90@hotmail.com>
2026-01-23 16:15:20 +0100

    finish 632

--- tasks/core-pro/632-extended-window/task.ts
+++ tasks/core-pro/632-extended-window/task.ts
@@ -1,5 +1,11 @@
 /* Rozszerz interfejs globalnego obiektu window o obiekt analytics z metodę trackEvent(string): void.
   Zrób to w sposób globalny, który będzie dostępny w całym projekcie, za pomocą dedykowanego pliku.
 */
-
+declare global {
+  interface Window {
+    analytics: {
+      trackEvent: (eventName: string) => void;
+    };
+  }
+}
 window.analytics.trackEvent('click');

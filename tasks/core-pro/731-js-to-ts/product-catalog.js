6064b78b87750c5ee275565f25ec2c5d032f0cf4
bmichalczuk <barmic90@hotmail.com>
2026-01-28 23:08:49 +0100

    finish 721

--- tasks/core-pro/731-js-to-ts/product-catalog.js
+++ tasks/core-pro/731-js-to-ts/product-catalog.js
@@ -1,5 +1,8 @@
 import { readFileSync } from 'fs';
+import ts from 'typescript';
 
 export function getProductCatalog() {
+  /** @returns {Array<{ id: number; name: string; price: number }>}
+   */
   return JSON.parse(readFileSync(new URL('./catalog.json', import.meta.url), 'utf8'));
 }
--- tasks/core-pro/731-js-to-ts/task.ts
+++ tasks/core-pro/731-js-to-ts/task.ts
@@ -4,5 +4,8 @@ export type CatalogProducts = ReturnType<typeof getProductCatalog>['products'];
 
 export function getTotalValue() {
   const catalog = getProductCatalog();
-  return catalog.products.reduce((acc, product) => acc + product.price, 0);
+  return catalog.products.reduce(
+    (acc: number, product: CatalogProducts[number]) => acc + product.price,
+    0,
+  );
 }
